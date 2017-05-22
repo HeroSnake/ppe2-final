745a8737ae795674f50f38bf9c44cff9b1e3185f
syx.florent <syx.florent@R20909>
2017-05-22 10:23:52 +0200

    Ajoutez des fichiers projet.

---
+++ WebPPE2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPPE2.MvcApplication" Language="C#" %>
---
+++ WebPPE2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPPE2.Startup))]
+namespace WebPPE2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
